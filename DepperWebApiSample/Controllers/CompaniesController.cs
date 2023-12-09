5270f06514543c60a308cd692bdda448dafe41e9
Cristian Solervicens <cscornely@gmail.com>
2023-12-09 16:40:29 -0300

    Se agrega Documentación a Swagger

--- DepperWebApiSample/Controllers/CompaniesController.cs
+++ DepperWebApiSample/Controllers/CompaniesController.cs
@@ -3,156 +3,219 @@ using DapperConsoleSample.Contracts;
 using DapperConsoleSample.Entities;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DepperWebApiSample.Controllers
+namespace DepperWebApiSample.Controllers;
+
+
+/// <summary>
+/// Api Compañías
+/// </summary>
+[Route("api/companies")]
+[ApiController]
+public class CompaniesController : ControllerBase
 {
+    private readonly ICompanyRepository _companyRepo;
     
-    [Route("api/companies")]
-    [ApiController]
-    public class CompaniesController : ControllerBase
+    public CompaniesController(ICompanyRepository companyRepo)
     {
-        private readonly ICompanyRepository _companyRepo;
-        
-        public CompaniesController(ICompanyRepository companyRepo)
-        {
-            _companyRepo = companyRepo;
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> GetCompanies()
-        {
-            try
-            {
-                var companies = await _companyRepo.GetCompanies();
-                return Ok(companies);
-            }
-            catch (Exception ex)
-            {
-                //log error
-                return StatusCode(500, ex.Message);
-            }
-        }
-
-        [HttpGet("{id}", Name = "CompanyById")]
-        public async Task<IActionResult> GetCompany(int id)
-        {
-            try
-            {
-                var company = await _companyRepo.GetCompany(id);
-                if (company == null)
-                    return NotFound();
-                return Ok(company);
-            }
-            catch (Exception ex)
-            {
-                //log error
-                return StatusCode(500, ex.Message);
-            }
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> CreateCompany(Company company)
-        {
-            try
-            {
-                var createdCompany = await _companyRepo.CreateCompany(company);
-                return CreatedAtRoute("CompanyById", new { id = createdCompany.Id }, createdCompany);
-            }
-            catch (Exception ex)
-            {
-                //log error
-                return StatusCode(500, ex.Message);
-            }
-        }
-
-        [HttpPut]
-        public async Task<IActionResult> UpdateCompany(Company company)
-        {
-            try
-            {
-                var dbCompany = await _companyRepo.GetCompany(company.Id);
-                if (dbCompany == null)
-                    return NotFound();
-
-                await _companyRepo.UpdateCompany(company);
-                return NoContent();
-            }
-            catch (Exception ex)
-            {
-                //log error
-                return StatusCode(500, ex.Message);
-            }
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteCompany(int id)
-        {
-            try
-            {
-                var dbCompany = await _companyRepo.GetCompany(id);
-                if (dbCompany == null)
-                    return NotFound();
-
-                await _companyRepo.DeleteCompany(id);
-                return NoContent();
-            }
-            catch (Exception ex)
-            {
-                //log error
-                return StatusCode(500, ex.Message);
-            }
-        }
-
-        [HttpGet("ByEmployeeId/{id}")]
-        public async Task<IActionResult> GetCompanyForEmployee(int id)
-        {
-            try
-            {
-                var company = await _companyRepo.GetCompanyByEmployeeId(id);
-                if (company == null)
-                    return NotFound();
-
-                return Ok(company);
-            }
-            catch (Exception ex)
-            {
-                //log error
-                return StatusCode(500, ex.Message);
-            }
-        }
-
-        [HttpGet("{id}/MultipleResult")]
-        public async Task<IActionResult> GetCompanyEmployeesMultipleResult(int id)
-        {
-            try
-            {
-                var company = await _companyRepo.GetCompanyEmployeesMultipleResults(id);
-                if (company == null)
-                    return NotFound();
-
-                return Ok(company);
-            }
-            catch (Exception ex)
-            {
-                //log error
-                return StatusCode(500, ex.Message);
-            }
-        }
-
-        [HttpGet("MultipleMapping")]
-        public async Task<IActionResult> GetCompaniesEmployeesMultipleMapping()
-        {
-            try
-            {
-                var company = await _companyRepo.GetCompaniesEmployeesMultipleMapping();
-
-                return Ok(company);
-            }
-            catch (Exception ex)
-            {
-                //log error
-                return StatusCode(500, ex.Message);
-            }
-        }
-
+        _companyRepo = companyRepo;
     }
+
+    /// <summary>
+    /// Retorna Todas las compañías.
+    /// </summary>
+    /// <returns>json</returns>
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetCompanies()
+    {
+        try
+        {
+            var companies = await _companyRepo.GetCompanies();
+            return Ok(companies);
+        }
+        catch (Exception ex)
+        {
+            //log error
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+    /// <summary>
+    /// Obtiene el detalle de una compañía por ID
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>json</returns>
+    [HttpGet("{id}", Name = "CompanyById")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetCompany(int id)
+    {
+        try
+        {
+            var company = await _companyRepo.GetCompany(id);
+            if (company == null)
+                return NotFound();
+            return Ok(company);
+        }
+        catch (Exception ex)
+        {
+            //log error
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+
+    /// <summary>
+    /// Crea una nueva Compañía
+    /// No se requiere el ID, este es retornado en el response
+    /// </summary>
+    /// <param name="company"></param>
+    /// <returns>json</returns>
+    /// <remarks>
+    /// Retorna el item creado con el Id interno que le fue asignado.
+    /// </remarks>
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public async Task<IActionResult> CreateCompany(Company company)
+    {
+        try
+        {
+            var createdCompany = await _companyRepo.CreateCompany(company);
+            return CreatedAtRoute("CompanyById", new { id = createdCompany.Id }, createdCompany);
+        }
+        catch (Exception ex)
+        {
+            //log error
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+
+    /// <summary>
+    /// Actualiza la Compañía identificada por el ID, todos los demás valores pueden cambiar.
+    /// </summary>
+    /// <param name="company"></param>
+    /// <returns></returns>
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> UpdateCompany(Company company)
+    {
+        try
+        {
+            var dbCompany = await _companyRepo.GetCompany(company.Id);
+            if (dbCompany == null)
+                return NotFound();
+
+            await _companyRepo.UpdateCompany(company);
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            //log error
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+
+    /// <summary>
+    /// Elimina la compañía identificada por su ID en la URL
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> DeleteCompany(int id)
+    {
+        try
+        {
+            var dbCompany = await _companyRepo.GetCompany(id);
+            if (dbCompany == null)
+                return NotFound();
+
+            await _companyRepo.DeleteCompany(id);
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+            //log error
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+
+    /// <summary>
+    /// Busca las compañías filtrándo por el ID de un Usuario.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>json</returns>
+    [HttpGet("ByEmployeeId/{id}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetCompanyForEmployee(int id)
+    {
+        try
+        {
+            var company = await _companyRepo.GetCompanyByEmployeeId(id);
+            if (company == null)
+                return NotFound();
+
+            return Ok(company);
+        }
+        catch (Exception ex)
+        {
+            //log error
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+
+    /// <summary>
+    /// Obtiene la colección de todos los Empleados.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>json</returns>
+    [HttpGet("{id}/MultipleResult")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetCompanyEmployeesMultipleResult(int id)
+    {
+        try
+        {
+            var company = await _companyRepo.GetCompanyEmployeesMultipleResults(id);
+            if (company == null)
+                return NotFound();
+
+            return Ok(company);
+        }
+        catch (Exception ex)
+        {
+            //log error
+            return StatusCode(500, ex.Message);
+        }
+    }
+
+
+    /// <summary>
+    /// Obtiene todas las compañias y sus empleados.
+    /// </summary>
+    /// <returns>json</returns>
+    [HttpGet("MultipleMapping")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetCompaniesEmployeesMultipleMapping()
+    {
+        try
+        {
+            var company = await _companyRepo.GetCompaniesEmployeesMultipleMapping();
+            return Ok(company);
+        }
+        catch (Exception ex)
+        {
+            //log error
+            return StatusCode(500, ex.Message);
+        }
+    }
+
 }
--- DepperWebApiSample/Program.cs
+++ DepperWebApiSample/Program.cs
@@ -3,15 +3,40 @@ using DapperConsoleSample.Contracts;
 using DapperConsoleSample.Repository;
 using DepperWebApiSample.Contracts;
 using DepperWebApiSample.Repository;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(setupAction =>
+{
+    var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+
+    //Aplica los comentarios C# de las API's ( con ///)
+    //Propiedades del Proyecto -> Build -> Output: Marcar casilla "Documentation File", dejar nombre por defecto. 
+    setupAction.IncludeXmlComments(xmlCommentsFullPath);
+    setupAction.SwaggerDoc("v1", new OpenApiInfo{
+                    Version = "v1",
+                    Title = "Dapper Web Api example",
+                    Description = "This is an ASP.NET Core Web API example for Dapper usage and Swagger Documentation."
+                }
+            );
+
+    //setupAction.AddSecurityDefinition("Dapper Web Api example Security", new OpenApiSecurityScheme()
+    //{
+    //	Type = SecuritySchemeType.Http,
+    //	Scheme = "Bearer";
+    //	Description = "Input a valid token to access this API"
+    //});
+});
+
 builder.Services.AddSingleton<DapperContext>();
 builder.Services.AddScoped<ICompanyRepository, CompanyRepository>();
 builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
